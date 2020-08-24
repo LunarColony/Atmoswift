b8d8cccb139e055c9c9efeba91e24359665886d3
LunarColony <yunisfarah4@gmail.com>
2020-08-24 17:56:47 +0100

    Initial Commit

---
+++ Atmoswift/AtmoswiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtmoswiftApp.swift
+//  Atmoswift
+//
+//  Created by Yunis Farah on 24/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct AtmoswiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
